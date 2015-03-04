fce234843dfae3d879a54d31aa3ab9684834f716
Konstantinos Daskalopoulos <k.t.daskalopoulos@gmail.com>
2015-03-04 11:58:22 +0000

    random heart beating

--- src/model/Pacemaker.java
+++ src/model/Pacemaker.java
@@ -2,4 +2,15 @@ package model;
 
 public class Pacemaker {
 
+    private static Pacemaker INSTANCE = new Pacemaker();
+
+    private Pacemaker()
+    {
+
+    }
+
+    public static Pacemaker getInstance()
+    {
+        return INSTANCE;
+    }
 }
