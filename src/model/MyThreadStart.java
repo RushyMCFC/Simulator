91f74f7a4853ded3efba70d55f93d429cbd76c6d
RushyMCFC <rushymcfc@gmail.com>
2015-03-04 16:27:53 +0000

    small change

--- src/model/MyThreadStart.java
+++ src/model/MyThreadStart.java
@@ -21,11 +21,11 @@ public class MyThreadStart {
 //		});
 //		t.start();
 
-        t = new Thread(new RunnableHeart());
-        t.start();
-        while(t.isAlive())
-        {
-            
-        }
+//        t = new Thread(new RunnableHeart());
+//        t.start();
+//        while(t.isAlive())
+//        {
+//            
+//        }
 	}
 }
